317d4e51f63924d1a1f87b2979d4b22527b034a7
Lukas Sassl <lusassl@microsoft.com>
2023-05-12 17:05:16 +0200

    Improve credential validation

--- Shared/ActiveDirectoryFunctions/Test-ADCredentials.ps1
+++ Shared/ActiveDirectoryFunctions/Test-ADCredentials.ps1
@@ -5,7 +5,7 @@
 
 function Test-ADCredentials {
     [CmdletBinding()]
-    [OutputType([bool])]
+    [OutputType([System.Object])]
     param(
         [Parameter(Mandatory = $true)]
         [System.Management.Automation.PSCredential]$Credentials,
@@ -14,9 +14,18 @@ function Test-ADCredentials {
         [ScriptBlock]$CatchActionFunction
     )
 
+    <#
+        This function tests whether the credentials provided are valid by trying to connect to LDAP server using Kerberos authentication.
+        It returns a PSCustomObject with two properties:
+        - UsernameFormat: "local", "upn" or "downlevel" depending on the format of the username provided
+        - CredentialsValid: $true if the credentials are valid, $false if they are not valid, $null if the function was unable to perform the validation
+    #>
+
     begin {
         Write-Verbose "Calling: $($MyInvocation.MyCommand)"
-        $credentialsValid = $false
+        $credentialsValid = $null
+        # Username formats: https://learn.microsoft.com/windows/win32/secauthn/user-name-formats
+        $usernameFormat = "local"
         try {
             Add-Type -AssemblyName System.DirectoryServices.Protocols -ErrorAction Stop
         } catch {
@@ -32,23 +41,38 @@ function Test-ADCredentials {
             if ($domain.Count -eq 2) {
                 Write-Verbose "Domain was extracted from UPN"
                 $domain = $domain[-1]
+                $usernameFormat = "upn"
             } else {
                 Write-Verbose "Failed to extract domain from UPN - seems that username was passed without domain and so cannot be validated"
                 $domain = $null
             }
+        } else {
+            Write-Verbose "Username was provided in down-level logon name format"
+            $usernameFormat = "downlevel"
         }
 
         if (-not([System.String]::IsNullOrEmpty($domain))) {
             $ldapDirectoryIdentifier = New-Object System.DirectoryServices.Protocols.LdapDirectoryIdentifier($domain)
+            # Use Kerberos authentication as NTLM might lead to false/positive results in case the password was changed recently
             $ldapConnection = New-Object -TypeName System.DirectoryServices.Protocols.LdapConnection($ldapDirectoryIdentifier, $Credentials, [DirectoryServices.Protocols.AuthType]::Kerberos)
+            # Enable Kerberos encryption (sign and seal)
             $ldapConnection.SessionOptions.Signing = $true
             $ldapConnection.SessionOptions.Sealing = $true
             try {
                 $ldapConnection.Bind()
                 Write-Verbose "Connection succeeded with credentials"
                 $credentialsValid = $true
+            } catch [System.DirectoryServices.Protocols.LdapException] {
+                if ($_.Exception.ErrorCode -eq 49) {
+                    # ErrorCode 49 means invalid credentials
+                    Write-Verbose "Failed to connect to LDAP server with credentials provided"
+                    $credentialsValid = $false
+                } else {
+                    Write-Verbose "Failed to connect to LDAP server for other reason"
+                }
+                Invoke-CatchActionError $CatchActionFunction
             } catch {
-                Write-Verbose "Failed to authenticate with credentials"
+                Write-Verbose "Exception occured while connecting to LDAP server - unable to perform credential validation"
                 Invoke-CatchActionError $CatchActionFunction
             }
         }
@@ -57,6 +81,9 @@ function Test-ADCredentials {
         if ($null -ne $ldapConnection) {
             $ldapConnection.Dispose()
         }
-        return $credentialsValid
+        return [PSCustomObject]@{
+            UsernameFormat   = $usernameFormat
+            CredentialsValid = $credentialsValid
+        }
     }
 }
